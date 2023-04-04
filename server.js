a51fa7033aaecdea3f6dc7ef1683ecf2fc393138
Festivemena <88979259+Festivemena@users.noreply.github.com>
2023-04-04 06:48:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ import connectDB from './mongodb/connect.js';
 
 dotenv.config();
 
+const port = "https://cedar-server.onrender.com";
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
@@ -27,7 +28,7 @@ const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
 
-    app.listen(8080, () => console.log('Server started on port http://localhost:8080'));
+    app.listen(port, () => console.log('Server started'));
   } catch (error) {
     console.log(error);
   }
